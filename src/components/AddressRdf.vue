4e6cf39073d21d9af02eecc15d6cab8eb9f085fa
pierre pracht <pierre.pracht@gmail.com>
2022-07-19 00:18:17 +0200

    format volar -> prettier ?

--- src/components/AddressRdf.vue
+++ src/components/AddressRdf.vue
@@ -1,18 +1,21 @@
 <template>
-    <address vocab="https://schema.org/" typeof="PostalAddress">
-        <span property="postalCode" v-text="addr.postalCode + ' '" />
-        <span v-if="!short" property="postOfficeBoxNumber" v-text="addr.postOfficeBoxNumber + ' '" />
-        <span property="streetAddress" v-text="addr.streetAddress + ' '" />
-        <span property="addressLocality" v-text="addr.addressLocality + ' '" />,
-        <span property="addressRegion" v-text="addr.addressRegion + ' '" />
-
-    </address>
+  <address vocab="https://schema.org/" typeof="PostalAddress">
+    <span property="postalCode" v-text="addr.postalCode + ' '" />
+    <span
+      v-if="!short"
+      property="postOfficeBoxNumber"
+      v-text="addr.postOfficeBoxNumber + ' '"
+    />
+    <span property="streetAddress" v-text="addr.streetAddress + ' '" />
+    <span property="addressLocality" v-text="addr.addressLocality + ' '" />,
+    <span property="addressRegion" v-text="addr.addressRegion + ' '" />
+  </address>
 </template>
 <script setup lang="ts">
 import { PostalAddress } from "../types";
 interface props {
-    addr: PostalAddress;
-    short: boolean;
+  addr: PostalAddress;
+  short: boolean;
 }
 defineProps<props>();
 </script>
