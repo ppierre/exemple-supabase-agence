1c687fcb998207a092f09a5820d239cb813593d3
pierre pracht <pierre.pracht@gmail.com>
2024-01-30 09:29:20 +0100

    casse sur nom

--- src/pages/liste-maisons.vue
+++ src/pages/liste-maisons.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import FicheOffreMaison from "../components/ficheOffreMaison.vue";
+import FicheOffreMaison from "../components/FicheOffreMaison.vue";
 import { supabase } from "../supabase";
 import { SchemaOffreMaison } from "../types";
 
